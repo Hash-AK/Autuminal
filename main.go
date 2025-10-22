cb3a93d65e431f3dc583dc20073cc87c3e27ec64
Hash-AK <175879022+Hash-AK@users.noreply.github.com>
2025-10-22 09:58:25 -0400

    adding screen clearing from a package foudn online

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/inancgumus/screen"
+)
+
+type Leaf struct {
+	X int
+	Y int
+}
+
 func main() {
+	fmt.Println("bob")
+	for {
+		screen.Clear()
+	}
 
 }
